1310d8e5e28d48137cad519a18dacbc2cd9efbdb
George Tsagkarelis <georgets98@gmail.com>
2022-05-04 23:06:39 +0300

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,43 @@
+const api = require('./config/api-url')
+var PROTO_PATH = __dirname + '/rpc/rpc.proto';
+var grpc = require('@grpc/grpc-js');
+var protoLoader = require('@grpc/proto-loader');
+var packageDefinition = protoLoader.loadSync(
+    PROTO_PATH,
+    {keepCase: true,
+     longs: String,
+     enums: String,
+     defaults: true,
+     oneofs: true
+    });
+
+var services = grpc.loadPackageDefinition(packageDefinition).services;
+
+const server = require('./server')
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+var nodeInfoClient = new services.NodeInfoService(api.apiUrl(), grpc.credentials.createInsecure())
+
+
+nodeInfoClient.GetSelfInfo(
+    {},
+    (err, res) => {
+        console.log('err:', err)
+        console.log('res:', res)
+    }
+)
+
+const start = async () => {
+    await sleep(1000)
+    try{
+        await server.startClient()
+    } catch (e) { 
+        console.log(e)
+    }
+}
+
+start()
+
---
+++ src/server.js
@@ -0,0 +1,115 @@
+const api = require('./config/api-url')
+const readline = require('readline');
+const prompt = require('prompt')
+
+var PROTO_PATH = __dirname + '/rpc/rpc.proto';
+var grpc = require('@grpc/grpc-js');
+var protoLoader = require('@grpc/proto-loader');
+const { exit } = require('process');
+const yaml = require('js-yaml')
+const fs = require('fs')
+var packageDefinition = protoLoader.loadSync(
+    PROTO_PATH,
+    {keepCase: true,
+     longs: String,
+     enums: String,
+     defaults: true,
+     oneofs: true
+    });
+
+var services = grpc.loadPackageDefinition(packageDefinition).services;
+
+var messageClient = new services.MessageService(api.apiUrl(), grpc.credentials.createInsecure())
+var discussionClient = new services.DiscussionService(api.apiUrl(), grpc.credentials.createInsecure())
+
+const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        terminal: false
+    });
+
+const findDiscussion = async (addr) => {
+    // let call = discussionClient.GetDiscussions({});
+    let disc = undefined
+    let prom = new Promise((resolve, reject) => {
+        discussionClient.GetDiscussions({})
+        .on('data', (res) => {
+            if(res.discussion.participants.length == 1
+            && res.discussion.participants[0] == addr) {
+                disc = res
+            }
+        })
+        .on('end', () => {
+            if(disc == undefined) {
+                discussionClient.AddDiscussion(
+                    {
+                        "discussion": {
+                            "participants": [
+                                addr
+                            ]
+                        }
+                    },
+                    (err, res) => {
+                        if(err)console.log(err)
+                        if(res){
+                            disc = res
+                            resolve(1)
+                        }
+                    }
+                )
+            } else {
+                resolve(1)
+            }
+        })
+    })
+    await Promise.all([prom])
+    return disc
+}
+
+const purchaseItem = async (disc, item) => {
+    let prom = new Promise((resolve, reject) => {
+        messageClient.sendMessage(
+            {
+                discussion_id: parseInt(disc.id),
+                payload: item,
+                amt_msat: parseInt(500000)
+            },
+            (err, res) => {
+                if(err){
+                    resolve(1)
+                }
+                if(res){
+                    messageClient.SubscribeMessages({})
+                    .on('data', (res) => {
+                        console.log('SUCCESSFULLY PURCHASED ITEM', item)
+                        console.log(res.received_message.payload)
+                    })
+                    .on('end', () => {
+                        resolve(1)
+                    })
+                }
+            }
+        )
+    })
+    await Promise.all([prom])
+}
+
+const startClient = async () => {
+    let doc
+    try {
+        doc = yaml.load(fs.readFileSync(__dirname + '/../config.yaml', 'utf8'));
+    } catch (e) {
+        console.log(e);
+    }
+    const disc = await findDiscussion(doc['service-node'].address)
+    console.log("Got disc: ", disc)
+    console.log("Enter name of item you want to purchase:")
+    rl.on('line', function(line){
+        if(line == "exit") exit(0)
+        purchaseItem(disc, line)
+    })
+}
+
+module.exports = {
+    startClient
+}
