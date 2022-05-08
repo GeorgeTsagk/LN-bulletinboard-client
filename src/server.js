d89e1d4f7cf982c480c9b30cbc3d7cba58ccc86d
George Tsagkarelis <georgets98@gmail.com>
2022-05-08 21:04:31 +0300

    Add command format with dot separator

--- src/server.js
+++ src/server.js
@@ -10,11 +10,12 @@ const yaml = require('js-yaml')
 const fs = require('fs')
 var packageDefinition = protoLoader.loadSync(
     PROTO_PATH,
-    {keepCase: true,
-     longs: String,
-     enums: String,
-     defaults: true,
-     oneofs: true
+    {
+        keepCase: true,
+        longs: String,
+        enums: String,
+        defaults: true,
+        oneofs: true
     });
 
 var services = grpc.loadPackageDefinition(packageDefinition).services;
@@ -23,63 +24,66 @@ var messageClient = new services.MessageService(api.apiUrl(), grpc.credentials.c
 var discussionClient = new services.DiscussionService(api.apiUrl(), grpc.credentials.createInsecure())
 
 const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-        terminal: false
-    });
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false
+});
 
 const findDiscussion = async (addr) => {
-    // let call = discussionClient.GetDiscussions({});
     let disc = undefined
     let prom = new Promise((resolve, reject) => {
         discussionClient.GetDiscussions({})
-        .on('data', (res) => {
-            if(res.discussion.participants.length == 1
-            && res.discussion.participants[0] == addr) {
-                disc = res
-            }
-        })
-        .on('end', () => {
-            if(disc == undefined) {
-                discussionClient.AddDiscussion(
-                    {
-                        "discussion": {
-                            "participants": [
-                                addr
-                            ]
+            .on('data', (res) => {
+                if (res.discussion.participants.length == 1
+                    && res.discussion.participants[0] == addr) {
+                    disc = res
+                }
+            })
+            .on('end', () => {
+                if (disc == undefined) {
+                    discussionClient.AddDiscussion(
+                        {
+                            "discussion": {
+                                "participants": [
+                                    addr
+                                ]
+                            }
+                        },
+                        (err, res) => {
+                            if (err) console.log(err)
+                            if (res) {
+                                disc = res
+                                resolve(1)
+                            }
                         }
-                    },
-                    (err, res) => {
-                        if(err)console.log(err)
-                        if(res){
-                            disc = res
-                            resolve(1)
-                        }
-                    }
-                )
-            } else {
-                resolve(1)
-            }
-        })
+                    )
+                } else {
+                    resolve(1)
+                }
+            })
     })
     await Promise.all([prom])
     return disc
 }
 
-const purchaseItem = async (disc, item) => {
+const sendRequest = async (disc, line) => {
+    const args = line.split(".")
+    if (args.length < 2) {
+        console.log("Please provide an amount of sats after the '.'")
+    }
     let prom = new Promise((resolve, reject) => {
         messageClient.sendMessage(
             {
                 discussion_id: parseInt(disc.id),
-                payload: item,
-                amt_msat: parseInt(500000)
+                payload: args[0],
+                amt_msat: parseInt(Number(args[1]) * 1000)
             },
             (err, res) => {
-                if(err){
+                if (err) {
                     resolve(1)
                 }
-                if(res){
-                    
+                if (res) {
+
                 }
             }
         )
@@ -87,16 +91,57 @@ const purchaseItem = async (disc, item) => {
     await Promise.all([prom])
 }
 
+const printHelp = () => {
+    console.log(
+        `
+        
+        Welcome to the bulletin board client!
+        
+        All the commands you type are requests transfered to another node in the network in the
+        form of micropayments.
+
+        The generic format for the commands is <command>.<amt>. The amount is a number,
+        representing the amount of sats transfered to the remote node per request.
+        i.e. "info.10" will send the request "info" with an attached amount of 10 sats.
+        
+
+        info        Acquire more info on bulletin board service node (default price for
+                    this request is 10 sat)
+
+        channel     Followed by a pubkey of a LN node. This request returns the total
+                    local balance of the remote node in all the channels with the node
+                    included in the request.
+                    Example
+                    "channel 0272d85b4c927c64f31bb0fee7a25a78c80e8c057d0de0c2fe779f086f64bf08bb.100"
+                    will return a number representing the total local balance of the remote node in
+                    all the channels with the node included in the request.
+
+        <item>      You can enter any item name, followed by its price, to purchase any other
+                    content that is up for sale by the remote node. To learn what items are
+                    available for sale, you need to first perform the "info" request.
+                    `
+    )
+}
+
 const startClient = async () => {
     messageClient.SubscribeMessages({})
-    .on('data', (res) => {
-        console.log('---------------------------')
-        console.log('SUCCESSFULLY PURCHASED ITEM')
-        console.log(res.received_message.payload)
-        console.log('---------------------------\n')        
-    })
-    .on('end', () => {
-    })
+        .on('data', (res) => {
+            console.log('---------------------------')
+            let response
+            if (res.received_message.payload.startsWith('{')) {
+                try {
+                    response = JSON.parse(res.received_message.payload)
+                } catch (e) {
+                    response = res.received_message.payload
+                }
+            } else {
+                response = res.received_message.payload
+            }
+            console.log(response)
+            console.log('---------------------------\n')
+        })
+        .on('end', () => {
+        })
     let doc
     try {
         doc = yaml.load(fs.readFileSync(__dirname + '/../config.yaml', 'utf8'));
@@ -104,10 +149,12 @@ const startClient = async () => {
         console.log(e);
     }
     const disc = await findDiscussion(doc['service-node'].address)
-    console.log("Enter name of item you want to purchase:")
-    rl.on('line', function(line){
-        if(line == "exit") exit(0)
-        purchaseItem(disc, line)
+    console.log("If you need help on how to use this client, type \"help\"")
+    console.log("Type a request for the remote node:")
+    rl.on('line', function (line) {
+        if (line == "exit") exit(0)
+        if (line == "help") printHelp()
+        else sendRequest(disc, line)
     })
 }
 
